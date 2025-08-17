befb20099ea696612705ec4122394b7b250708a2
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-08-17 13:54:58 +0530

    Add EmptyChat and EmptyChatMessageInput components for improved chat interface; implement MessageBox and Dialog components for message display and modal functionality.

---
+++ interface/components/MessageBox.tsx
@@ -0,0 +1,56 @@
+import { MutableRefObject, useState } from "react";
+import { Message } from "./ChatWindow";
+import { cn } from "@/lib/utils";
+import { BookCopy } from "lucide-react";
+import MessageSources from "./MessageSources";
+
+const MessageBox = ({
+  message,
+  messageIndex,
+  history,
+  loading,
+  dividerRef,
+  isLast,
+  rewrite,
+}: {
+  message: Message;
+  messageIndex: number;
+  history: Message[];
+  loading: boolean;
+  dividerRef: MutableRefObject<HTMLDivElement | null>;
+  isLast: boolean;
+  rewrite: (messageId: string) => void;
+}) => {
+  const [parsedMessage, setParsedMessage] = useState(message.content);
+  return (
+    <>
+      {message.role === "user" && (
+        <div className={cn("w-full", messageIndex === 0 ? "pt-16" : "pt-8")}>
+          <h2 className="text-white font-medium text-3xl lg:w-9/12">
+            {message.content}
+          </h2>
+        </div>
+      )}
+      {message.role === "assistant" && (
+        <div className="flex flex-col space-y-9 lg:space-y-0 lg:flex-row lg:justify-between lg:space-x-9">
+          <div
+            ref={dividerRef}
+            className="flex flex-col space-y-6 w-full lg:w-9/12"
+          >
+            {message.sources && message.sources.length > 0 && (
+              <div className="flex flex-col space-y-2">
+                <div className="flex flex-row items-center space-x-2">
+                  <BookCopy className="text-white" size={20} />
+                  <h3 className="text-white font-medium text-xl">Sources</h3>
+                </div>
+                <MessageSources sources={message.sources} />
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default MessageBox;
