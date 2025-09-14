85a3ba44ef0136f1336f21db34a3977bf11de0a2
mrkeshav.05 <keshavpundhir69@gmail.com>
2025-09-14 13:13:17 +0530

    Fix formatting in formatTimeDifference function for consistency

--- interface/lib/utils.ts
+++ interface/lib/utils.ts
@@ -15,18 +15,18 @@ export const formatTimeDifference = (date1: Date, date2: Date): string => {
   } else if (diffInSeconds < 3600) {
     return `${Math.floor(diffInSeconds / 60)} minute${
       Math.floor(diffInSeconds / 3600) !== 1 ? "s" : ""
-    }}`;
+    }`;
   } else if (diffInSeconds < 86400) {
     return `${Math.floor(diffInSeconds / 3600)} hour${
       Math.floor(diffInSeconds / 3600) !== 1 ? "s" : ""
-    }}`;
+    }`;
   } else if (diffInSeconds < 31536000) {
     return `${Math.floor(diffInSeconds / 86400)} day${
       Math.floor(diffInSeconds / 86400) !== 1 ? "s" : ""
-    }}`;
+    }`;
   } else {
     return `${Math.floor(diffInSeconds / 31536000)} year${
       Math.floor(diffInSeconds / 31536000) !== 1 ? "s" : ""
-    }}`;
+    }`;
   }
 };
